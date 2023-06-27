4c127b8a8a371179fa3b12f1a5a16b1566f6b3d0
Vinura Perera <vinurap4@gmail.com>
2023-06-27 13:28:15 +0530

    updated to retrieval qa

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
 from langchain.chains import RetrievalQA
 
 from pdf_reader import load_pdf
@@ -19,19 +18,18 @@ def get_cover_letter(url, pdf):
     documents = split_text_documents(pdf_doc)
 
     vectordb = Chroma.from_documents(documents, embedding=OpenAIEmbeddings())
-    # vectordb.persist() perssit only the CV, otherone dynamic
 
-    # create our Q&A chain
-    pdf_qa = ConversationalRetrievalChain.from_llm(
+    pdf_qa = RetrievalQA.from_chain_type(
         ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo'),
         retriever=vectordb.as_retriever(search_kwargs={'k': 6}),
-        return_source_documents=True,
-        verbose=False
+        chain_type="stuff",
     )
 
-    query = 'write an overview for upwork Describe my strengths and skills Highlight projects, accomplishments and education using the given CV'
-    chat_history = []
+    query = 'Write a cover letter for given CV and Job posting in a conversational style and fill out the writers name in the end using cv'
 
-    result = pdf_qa({"question": query, "chat_history": chat_history})
+    result = pdf_qa.run(query)
 
-    return result["answer"]
+    return result
+
+z = get_cover_letter('https://www.linkedin.com/jobs/view/3643170283/?alternateChannel=search&refId=q%2BNGner5UrCvWUq9ICmjJA%3D%3D&trackingId=biOFknFRaCIGG8E39mHyOA%3D%3D', './cv.pdf')
+print(z)
